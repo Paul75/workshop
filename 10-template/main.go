42a80205298cf3ee18e13df221128e1721867d21
Henri LEPIC <henrilepic@gmail.com>
2020-12-03 10:19:22 +0100

    update comment

--- 10-template/main.go
+++ 10-template/main.go
@@ -10,15 +10,13 @@ type User struct {
 	Age       uint
 }
 
-// 1- implement the stringer interface of fmt for User.
-
 func main() {
-	// 2- define a variable name u of type User with its fields.
+	// 1- define a variable name u of type User with its fields.
 	tmpl, err := template.New("test").Parse("Hello my name is {{.FirstName}} and I'm {{.Age}} years old.")
 	if err != nil {
 		panic(err)
 	}
-	// 3- replace the expression nil with u.
+	// 2- replace the expression nil with u.
 	err = tmpl.Execute(os.Stdout, nil)
 	if err != nil {
 		panic(err)
